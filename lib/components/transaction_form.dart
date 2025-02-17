8c027fc1b7e13dc98f0ec8f969c2ea8ff372bcb5
Fellipe Nascimento Guedes <fngprogrammer@hotmail.com>
2025-02-17 08:32:54 -0300

    feat: creates project

---
+++ lib/components/transaction_form.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class TransactionForm extends StatelessWidget {
+  TransactionForm(this.onSubmit, {super.key});
+
+  final titleController = TextEditingController();
+  final valueController = TextEditingController();
+
+  final void Function(String, double) onSubmit;
+
+  _submitForm() {
+    final title = titleController.text;
+    final value = double.tryParse(valueController.text) ?? 0;
+    if (title.isEmpty || value <= 0) {
+      return;
+    }
+    onSubmit(title, value);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: titleController,
+              onSubmitted: (_) => _submitForm(),
+              decoration: InputDecoration(labelText: 'Título'),
+            ),
+            TextField(
+              controller: valueController,
+              decoration: InputDecoration(labelText: 'Valor (R\$)'),
+              onSubmitted: (_) => _submitForm(),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+            ),
+            TextButton(
+                child: Text('Nova Transação',
+                    style: TextStyle(
+                        color: Colors.purple,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18)),
+                onPressed: _submitForm),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,11 @@
+import 'package:despesas/components/transaction_form.dart';
+import 'package:despesas/components/transaction_user.dart';
+import 'package:despesas/models/transaction.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
+
+import 'package:despesas/components/transaction_form.dart';
+import 'package:despesas/components/transaction_list.dart';
 
 main() => runApp(ExpensesApp());
 
@@ -11,13 +18,61 @@ class ExpensesApp extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _transaction = [
+    Transaction(id: '1', title: 'privada', value: 500, date: DateTime.now()),
+    Transaction(id: '2', title: 'casa0', value: 453, date: DateTime.now()),
+    Transaction(id: '2', title: 'casa1', value: 453, date: DateTime.now()),
+    Transaction(id: '2', title: 'casa2', value: 453, date: DateTime.now()),
+    Transaction(id: '2', title: 'casa3', value: 453, date: DateTime.now()),
+    Transaction(id: '2', title: 'casa4', value: 453, date: DateTime.now()),
+    Transaction(id: '2', title: 'casa5', value: 453, date: DateTime.now()),
+  ];
+
+  _opentransactionFormModal(BuildContext context) {
+    showModalBottomSheet(
+        context: context,
+        builder: (_) {
+          return TransactionForm();
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('data'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Despesas"),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child:
+            Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          Card(
+            elevation: 5,
+            color: Colors.blue,
+            child: Text('card 1'),
+          ),
+          TransactionUser(),
+        ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      // floatingActionButton(FloatingActionButton(child: Icon(Icons.add)))
     );
   }
 }
